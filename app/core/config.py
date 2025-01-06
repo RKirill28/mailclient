0a645a7c9925eced69963100530f15df2e759986
Roca <pavlov26.2008@gmail.com>
2025-01-06 17:14:38 +0700

    Add config for database

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,30 @@
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
+class DatabaseConfig(BaseModel):
+	host: str = 'postgres'
+	port: int = 5432
+	db_name: str = 'email'
+	db_user: str = 'postgres'
+	db_pass: str = 'postgres'
+	url: str = f'postgresql+asyncpg://{db_user}:{db_pass}@{host}:{port}/{db_name}"'
+
+class SQLAlchemyConfig(BaseModel):
+	echo: bool = True
+	echo_pool: bool = False
 
 class RunConfig(BaseModel):
 	host: str = '0.0.0.0'
 	port: int = 8000
 
-
 class ApiPrefixConfig(BaseModel):
 	api_prefix: str = '/api'
-
 
 class Settings(BaseSettings):
 	run: RunConfig = RunConfig()
 	api: ApiPrefixConfig = ApiPrefixConfig()
+	database: DatabaseConfig = DatabaseConfig()
+	sqla: SQLAlchemyConfig = SQLAlchemyConfig()
 
 
 settings = Settings()
