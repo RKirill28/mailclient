cfa1ac2519b14efb991e8e5fcb3547a945b84454
Roca <pavlov26.2008@gmail.com>
2025-01-06 17:15:43 +0700

    Create DatabaseHelper class

---
+++ app/core/models/db_helper.py
@@ -0,0 +1,39 @@
+from typing import AsyncGenerator
+
+from core.config import settings
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker, AsyncSession
+
+
+class DatabaseHelper:
+	def __init__(
+			self, 
+			url: str, 
+			echo: bool,
+			echo_pool: bool
+		):
+		self.engine: AsyncEngine = create_async_engine(
+			url=url,
+			echo=echo,
+			echo_pool=echo_pool
+		)
+		self.sessionFactory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+			bind=self.engine,
+			autoflush=False,
+			autocommit=False,
+			expire_on_commit=False
+		) # async_sessionmaker is class
+	
+	async def dispose(self):
+		await self.engine.dispose()
+
+	async def session_getter(self) -> AsyncGenerator[AsyncSession]:
+		async with self.sessionFactory() as session:
+			yield session
+
+
+dbHelper = DatabaseHelper(
+	url=settings.database.url,
+	echo=settings.sqla.echo,
+	echo_pool=settings.sqla.echo_pool
+)
